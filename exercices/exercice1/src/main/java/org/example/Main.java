122d58d041b20e13ff44e8f4d8d39996daecae3c
guillaume806 <guillaumebodart@outlook.fr>
2023-08-17 09:59:39 +0200

    exo1

--- exercices/exercice1/src/main/java/org/example/Main.java
+++ exercices/exercice1/src/main/java/org/example/Main.java
@@ -3,79 +3,49 @@ package org.example;
 import org.example.utlis.DatabaseManager;
 
 import java.sql.*;
+import java.util.Date;
 import java.util.Scanner;
+
+import static org.example.Student.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        Connection connection = null;
-        try {
-            connection = DatabaseManager.getPostgreSQLConnection();
-            System.out.println("Merci de saisir le prénom :");
-            String firstname = scanner.nextLine();
-            System.out.println("Merci de saisir le nom :");
-            String lastname = scanner.nextLine();
-            System.out.println("Merci de saisir le numero de classe :");
-            String classnumber = scanner.nextLine();
-            System.out.println("Merci de saisir la date de diplome :");
-            String diplomedate = scanner.nextLine();
 
-            String request = "INSERT INTO person (firstname, lastname, classnumber, diplomedate) VALUES ( ? , ? , ? , ?)";
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            while (true) {
+                System.out.println("Menu:");
+                System.out.println("1. Ajouter un étudiant");
+                System.out.println("2. Afficher tous les étudiants");
+                System.out.println("3. Afficher les étudiants d'une classe");
+                System.out.println("4. Supprimer un étudiant");
+                System.out.println("5. Quitter");
+                System.out.print("Choix : ");
+                Scanner scanner;
+                int choice = scanner.nextInt();
 
-
-
-            // Facon 2 => avec une requête préparée
-
-            PreparedStatement preparedStatement = connection.prepareStatement(request);
-            preparedStatement.setString(1,firstname);
-            preparedStatement.setString(2,lastname);
-            preparedStatement.setString(2,classnumber);
-            preparedStatement.setString(2,diplomedate);
-            //preparedStatement.execute(); // retour boolean
-            int nbRows = preparedStatement.executeUpdate();
-            System.out.println("Nombre ligne "+nbRows);
-
-
-            // version avec recuperation de l'id auto genere
-
-            /*
-            PreparedStatement preparedStatement = connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1,firstname);
-            preparedStatement.setString(2,lastname);
-            int nbRows = preparedStatement.executeUpdate();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            System.out.println("Nombre ligne "+nbRows);
-            if(resultSet.next()){
-                System.out.println(resultSet.getInt(1));
-            }
-
-             */
-
-            // facon 3 => avec une requete de lecture
-            String request = "SELECT * FROM person";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(request);
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt("id")+ " , "+ resultSet.getString("first_name")+
-                        " , "+ resultSet.getString("last_name"));
-            }
-
-
-
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
-        }finally {
-            if(connection != null){
-                try{
-                    connection.close();
-                }catch (SQLException e){
-                    e.printStackTrace();
+                switch (choice) {
+                    case 1:
+                        addEtudiant(connection, scanner);
+                        break;
+                    case 2:
+                        listAllEtudiants(connection);
+                        break;
+                    case 3:
+                        listEtudiantsByClasse(connection, scanner);
+                        break;
+                    case 4:
+                        deleteEtudiant(connection, scanner);
+                        break;
+                    case 5:
+                        System.out.println("Au revoir !");
+                        return;
+                    default:
+                        System.out.println("Choix invalide.");
                 }
             }
         }
-        }
+
     }
-}
---
+++ exercices/exercice1/src/main/java/org/example/Student.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Student {
+
+    private String nom;
+
+    private String prenom;
+
+    private String classe;
+
+    private String dateDiplome;
+
+
+    public static void addEtudiant(Connection connection, Scanner scanner) throws SQLException {
+        System.out.print("Nom : ");
+        String nom = scanner.next();
+        System.out.print("Prénom : ");
+        String prenom = scanner.next();
+        System.out.print("Numéro de classe : ");
+        String classe = scanner.next();
+        System.out.print("Date de diplôme (AAAA-MM-JJ) : ");
+        String dateDiplome = scanner.next();
+
+        String query = "INSERT INTO etudiant (nom, prenom, classe, date_diplome) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, nom);
+            preparedStatement.setString(2, prenom);
+            preparedStatement.setString(3, classe);
+            preparedStatement.setDate(4, Date.valueOf(dateDiplome));
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Étudiant ajouté avec succès.");
+            } else {
+                System.out.println("Erreur lors de l'ajout de l'étudiant.");
+            }
+        }
+    }
+
+    public static void listAllEtudiants(Connection connection) throws SQLException {
+        String query = "SELECT * FROM etudiant";
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nom = resultSet.getString("nom");
+                String prenom = resultSet.getString("prenom");
+                String classe = resultSet.getString("classe");
+                Date dateDiplome = resultSet.getDate("date_diplome");
+
+                System.out.println("ID : " + id);
+                System.out.println("Nom : " + nom);
+                System.out.println("Prénom : " + prenom);
+                System.out.println("Classe : " + classe);
+                System.out.println("Date de diplôme : " + dateDiplome);
+                System.out.println();
+            }
+        }
+    }
+
+    public static void listEtudiantsByClasse(Connection connection, Scanner scanner) throws SQLException {
+        System.out.print("Numéro de classe : ");
+        String classe = scanner.next();
+
+        String query = "SELECT * FROM etudiant WHERE classe = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, classe);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String nom = resultSet.getString("nom");
+                    String prenom = resultSet.getString("prenom");
+                    Date dateDiplome = resultSet.getDate("date_diplome");
+
+                    System.out.println("ID : " + id);
+                    System.out.println("Nom : " + nom);
+                    System.out.println("Prénom : " + prenom);
+                    System.out.println("Date de diplôme : " + dateDiplome);
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void deleteEtudiant(Connection connection, Scanner scanner) throws SQLException {
+        System.out.print("ID de l'étudiant à supprimer : ");
+        int id = scanner.nextInt();
+
+        String query = "DELETE FROM etudiant WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Étudiant supprimé avec succès.");
+            } else {
+                System.out.println("Aucun étudiant trouvé avec cet ID.");
+            }
+        }
+    }
+}
